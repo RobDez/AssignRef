8031fb3cfd75695757e7490e124cf96523241608
Roberto Fernandez <116823051+RobDez@users.noreply.github.com>
2023-08-02 15:33:45 -0500

    Update CertificationResultAddRequest.cs

--- Product-code-showcase/DotNet/CertificationResultAddRequest.cs
+++ Product-code-showcase/DotNet/CertificationResultAddRequest.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests.CertificationResults
+namespace Models.Requests.CertificationResults
 {
     public class CertificationResultAddRequest
     {
