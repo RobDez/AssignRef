7f406f136f51ee4f4d2aa9ca9876ae6ff1a6179e
Roberto Fernandez <116823051+RobDez@users.noreply.github.com>
2023-08-02 14:40:19 -0500

    Update CertificationResult.cs

--- Product-code-showcase/DotNet/CertificationResult.cs
+++ Product-code-showcase/DotNet/CertificationResult.cs
@@ -1,4 +1,4 @@
-﻿using Sabio.Models.Domain.Certifications;
+﻿using Models.Domain.Certifications;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Domain.CertificationResults
+namespace Models.Domain.CertificationResults
 {
     public class CertificationResult
     {
